302a8d26d97615dbf4df60fadf337effcb7a1d9e
jamesnottidge <freshice57@gmail.com>
2023-10-17 12:52:58 +0100

    log

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -13,6 +13,7 @@ export const createNewUser = async (req, res, next) => {
     const token = createJWT(user);
     res.json({ token });
   } catch (e) {
+    console.log(e);
     e.type = "input";
     next(e);
   }
